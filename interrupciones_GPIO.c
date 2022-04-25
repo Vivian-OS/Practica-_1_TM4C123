d808c0d92ec24b982929c6bd5abbd69d5071e847
Vivian-Salinas <101617965+Vivian488@users.noreply.github.com>
2022-04-25 12:08:21 -0500

    Update interrupciones_GPIO.c

--- interrupciones_GPIO.c
+++ interrupciones_GPIO.c
@@ -1,7 +1,7 @@
 //GPIO interrupciones
 //Vivian Odette Salinas Benavídez
 //Microcontroladores
-
+//Usar puerto E para interrupciones
 
 #include "lib/include.h"
 extern void Configurar_GPIO_E (void){
